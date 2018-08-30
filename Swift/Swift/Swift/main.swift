41b6e345605d707c901aa94b4a145afa0afc4bbb
Vineet Choudhary <vineetchoudhary@live.in>
2018-08-30 23:29:34 +0530

    Swift Console App Added

---
+++ Swift/Swift/Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift
+//
+//  Created by Vineet Choudhary on 30/08/18.
+//  Copyright © 2018 Developer Insider. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
