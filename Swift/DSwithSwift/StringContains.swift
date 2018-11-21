59e073642be4a53db108c3919376d2825c01347a
Vineet Choudhary <vineetchoudhary@live.in>
2018-11-21 23:52:48 +0530

    Updated with more extension function

--- Swift/DSwithSwift/StringContains.swift
+++ Swift/DSwithSwift/StringContains.swift
@@ -1,6 +1,38 @@
 import Foundation
 
 extension String {
+    subscript (i: Int)-> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (bounds: CountableRange<Int>)-> Substring {
+        let startIndex = index(self.startIndex, offsetBy: bounds.upperBound)
+        let endIndex = index(self.endIndex, offsetBy: bounds.lowerBound)
+        return self[startIndex ..< endIndex]
+    }
+    
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ..< end]
+    }
+    
+    subscript (bounds: CountableClosedRange<Int>)-> Substring {
+        let startIndex = index(self.startIndex, offsetBy: bounds.upperBound)
+        let endIndex = index(self.endIndex, offsetBy: bounds.lowerBound)
+        return self[startIndex ... endIndex]
+    }
+    
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        return self[start ... end]
+    }
+    
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ... end]
+    }
+    
     public func isContain(string: String)-> Bool {
         if self.count < string.count || string.isEmpty{
             return false
@@ -8,11 +40,11 @@ extension String {
         
         var matchIndex: Int = 0
         for ch in self {
-            if ch == string[string.index(string.startIndex, offsetBy: matchIndex)] {
+            if ch == string[matchIndex] {
                 matchIndex += 1
             } else if (matchIndex > 0) {
                 matchIndex = 0
-                if ch == string[string.index(string.startIndex, offsetBy: matchIndex)] {
+                if ch == string[matchIndex] {
                     matchIndex += 1
                 }
             }
