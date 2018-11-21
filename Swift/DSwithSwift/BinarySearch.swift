d57704b8dfd44e506b94410843093f3501ab741d
Vineet Choudhary <vineetchoudhary@live.in>
2018-11-22 00:21:56 +0530

    BS

---
+++ Swift/DSwithSwift/BinarySearch.swift
@@ -0,0 +1,33 @@
+//
+//  BinarySearch.swift
+//  DSwithSwift
+//
+//  Created by Vineet Choudhary on 22/11/18.
+//  Copyright © 2018 Developer Insider. All rights reserved.
+//
+
+import Foundation
+
+func BinarySearch<T: Comparable>(array: [T], item: T, range: Range<Int>) -> Int? {
+    if range.lowerBound >= range.upperBound {
+        return nil
+    } else {
+        let midIndex = range.lowerBound + ((range.upperBound - range.lowerBound) / 2)
+        if array[midIndex] < item {
+            return BinarySearch(array: array, item: item, range: (midIndex + 1) ..< range.upperBound)
+        } else if array[midIndex] > item {
+            return BinarySearch(array: array, item: item, range: range.lowerBound ..< midIndex)
+        } else {
+            return midIndex
+        }
+    }
+}
+
+func startBinarySearch() {
+    let date = Date()
+    print(Date().timeIntervalSince(date))
+    let array = Array(1...9999999)
+    print(Date().timeIntervalSince(date))
+    print(BinarySearch(array: array, item: 975689, range: 0..<array.count) ?? -1)
+    print(Date().timeIntervalSince(date))
+}
